30953c7c31d098e39092d076bb1f9487d6039325
patidarayush78 <ayushpatidar7898@gmail.com>
2021-10-07 18:09:49 +0530

    second commit

--- jugal/settings.py
+++ jugal/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
 MEDIA_ROOT=BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

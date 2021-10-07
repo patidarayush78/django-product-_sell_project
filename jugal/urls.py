cfc3127ef951e3bef7427eecb1dfb738944b4b52
patidarayush78 <ayushpatidar7898@gmail.com>
2021-10-07 19:19:52 +0530

    second commit

--- jugal/urls.py
+++ jugal/urls.py
@@ -94,9 +94,9 @@ urlpatterns = [
 
 
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-     
+    url(r'^$', 'path.to.function') 
 
 
 
